0e4c45061e04f70c6bfb097a83ed4fb231b0e61d
Martin Bruse <martin@oort.se>
2012-06-12 16:31:07 +0200

    more understanding!

--- chicklet_test.go
+++ chicklet_test.go
@@ -7,20 +7,23 @@ import (
 
 func TestSimpleEvaluation(t *testing.T) {
 	c := NewContext()
-	s := "1 + 2"
+	s := "func() int { return 1 + 2 }()"
 	code, err := c.Compile(s)
-	if err != nil {
-		t.Error(s, "should compile")
-	}
-	val, err := code.Run()
-	if err != nil {
-		t.Error(s, "should run")
-	}
-	v, ok := val.(IdealIntValue); 
-	if !ok {
-		t.Error(s, "should generate an IdealIntValue")
-	}
-	if v.Get().Int64() != 3 {
-		t.Error(s, "should generate 3")
+	if err == nil {
+		val, err := code.Run()
+		if err == nil {
+			v, ok := val.(*intV)
+			if ok {
+				if *v != 3 {
+					t.Error(s, "should generate 3")
+				}
+			} else {
+				t.Error(s, "should generate an int")
+			}
+		} else {
+			t.Error(s, "should run")
+		}
+	} else {
+		t.Error(s, "should compile, got", err)
 	}
 }
