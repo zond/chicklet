ac1f7a42477626c8ffc97848ed8f12125d8c2bd8
Martin Bruse <martin@oort.se>
2012-06-13 10:35:45 +0200

    more awesomeness

--- chicklet_test.go
+++ chicklet_test.go
@@ -13,6 +13,9 @@ func TestIntReturn(t *testing.T) {
 	if err == nil {
 		val, err := code.Call()
 		if err == nil {
+			if len(val) != 1 {
+				t.Error(s, "should generate one value, generated", len(val))
+			}
 			if val[0].(int) != 3 {
 				t.Error(s, "should generate 3")
 			}
@@ -31,6 +34,9 @@ func TestStringReturn(t *testing.T) {
 	if err == nil {
 		val, err := code.Call()
 		if err == nil {
+			if len(val) != 1 {
+				t.Error(s, "should generate one value, generated", len(val))
+			}
 			if val[0].(string) != "bla" {
 				t.Error(s, "should generate \"bla\" but generated", val[0])
 			}
@@ -49,6 +55,9 @@ func TestIdealFloatReturn(t *testing.T) {
 	if err == nil {
 		val, err := code.Call()
 		if err == nil {
+			if len(val) != 1 {
+				t.Error(s, "should generate one value, generated", len(val))
+			}
 			cmp := big.NewRat(41, 10)
 			if cmp.Cmp(val[0].(*big.Rat)) != 0 {
 				t.Error(s, "should generate", cmp, "but generated", val[0])
@@ -68,6 +77,9 @@ func TestBoolReturn(t *testing.T) {
 	if err == nil {
 		val, err := code.Call()
 		if err == nil {
+			if len(val) != 1 {
+				t.Error(s, "should generate one value, generated", len(val))
+			}
 			cmp := true
 			if val[0].(bool) != cmp {
 				t.Error(s, "should generate", cmp, "but generated", val[0])
@@ -80,34 +92,57 @@ func TestBoolReturn(t *testing.T) {
 	}
 }
 
-/*
-func TestFunctionReturn(t *testing.T) {
+func TestFunc0_0Return(t *testing.T) {
 	c := NewContext()
-	s := "func() int { return 1 + 2 }"
+	s := "func() {}"
 	code, err := c.Compile(s)
 	if err == nil {
-		val, err := code.Run()
+		val, err := code.Call()
 		if err == nil {
-			fmt.Printf("%v\n", val)
-
-			 v, ok := val.(*funcV)
-			 if ok {
-			 t := &Thread{}
-			 fun := v.Get(t)
-			 f := fun.NewFrame()
-			 
-			 t.f = f
-			 fun.Call(t)
-			 fmt.Println(t.f)
-						} else {
-				t.Error(s, "should generate a function")
+			if len(val) != 1 {
+				t.Error(s, "should generate one value, generated", len(val))
 			}
-
+			rval, err := val[0].(Callable).Call()
+			if err == nil {
+				if len(rval) != 0 {
+					t.Error(s, "should not return anything when called, returned", rval)
+				}
+			} else {
+				t.Error(s, "should be callable, got", err)
+			}
 		} else {
-			t.Error(s, "should run")
+			t.Error(s, "should run, got", err)
 		}
 	} else {
 		t.Error(s, "should compile, got", err)
 	}
 }
-*/
+
+func TestFunc0_1Return(t *testing.T) {
+	c := NewContext()
+	s := "func() int { return 1 }"
+	code, err := c.Compile(s)
+	if err == nil {
+		val, err := code.Call()
+		if err == nil {
+			if len(val) != 1 {
+				t.Error(s, "should generate one value, generated", len(val))
+			}
+			rval, err := val[0].(Callable).Call()
+			if err == nil {
+				if len(rval) != 1 {
+					t.Error(s, "should not return one value when called, returned", len(rval))
+				}
+				if rval[0].(int) != 1 {
+					t.Error(s, "should return 1 when called, returned", rval[0])
+				}
+			} else {
+				t.Error(s, "should be callable, got", err)
+			}
+		} else {
+			t.Error(s, "should run, got", err)
+		}
+	} else {
+		t.Error(s, "should compile, got", err)
+	}
+}
--- context.go
+++ context.go
@@ -50,6 +50,45 @@ func (self *ConvertError) Error() string {
 	return self.Message
 }
 
+type CallError struct {
+	Message string
+}
+func (self *CallError) Error() string {
+	return self.Message
+}
+
+type EvalFuncWrapper struct {
+	target *evalFunc
+}
+func (self *EvalFuncWrapper) Call(things... Thing) (rval []Thing, err error) {
+	if len(things) != len(self.target.inTypes) {
+		return nil, &CallError{fmt.Sprint("Wrong number of arguments. Wanted", len(self.target.inTypes), "but got", len(things))}
+	}
+	frame := self.target.NewFrame()
+	for index, thing := range things {
+		val, err := convertOne(thing)
+		if err != nil {
+			return nil, err
+		}
+		frame.Vars[index] = val.(Value)
+	}
+	for index, t := range self.target.outTypes {
+		frame.Vars[len(self.target.inTypes) + index] = t.(*NamedType).Zero()
+	}
+	thread := &Thread{}
+	thread.f = frame
+	self.target.Call(thread)
+	for index, _ := range self.target.outTypes {
+		val := frame.Vars[len(self.target.inTypes) + index]
+		converted, err := convertOne(val)
+		if (err != nil) {
+			return nil, err
+		}
+		rval = append(rval, converted)
+	}
+	return rval, nil
+}
+
 func convert(things... Thing) (rval []Thing, err error) {
 	for _, t := range things {
 		c, err := convertOne(t)
@@ -68,6 +107,10 @@ func convertOne(t Thing) (rval Thing, err error) {
 	case *stringV: return string(*(t.(*stringV))), nil
 	case *idealFloatV: return (*(t.(*idealFloatV))).Get(), nil
 	case *boolV: return bool(*(t.(*boolV))), nil
+	case *funcV: 
+		switch t.(*funcV).target.(type) {
+		case *evalFunc: return &EvalFuncWrapper{t.(*funcV).target.(*evalFunc)}, nil
+		}
 	}
 	return nil, &ConvertError{fmt.Sprintf("Unable to convert %v of type %T", t, t)}
 }
