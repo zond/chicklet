07534dcf7c2d5d8e8ce690d5c862ab5003d03198
Martin Bruse <martin@oort.se>
2012-06-15 08:26:59 +0200

    updated example

--- example/example.go
+++ example/example.go
@@ -5,16 +5,17 @@ import (
 	"fmt"
 )
 
-func nativeTest(i int64, s string) int {
+func nativeTest(i int, s string) int {
 	fmt.Println("nativeTest called with", i, s)
-	return int(i) + len(s)
+	return i + len(s)
 }
 
 func main() {
 	context := chicklet.NewWorld()
 	context.Define("myNative", nativeTest)
-	context.Eval("func callNative(s string, i int64) int { return myNative(i, s) }")
-	evalFunc := context.Eval("func(s string, i int64) (int, string) { return callNative(s, i), s }").(chicklet.Executable)
-	r, _ := evalFunc.Execute("hello world", 17)
+	context.Eval("func callNative(s string, i int) int { return myNative(i, s) }")
+	r, _ := context.Eval("func(s string, i int) (int, string) { return callNative(s, i), s }")
+	evalFunc := r.(chicklet.Executable)
+	r, _ = evalFunc.Execute("hello world", 17)
 	fmt.Printf("result is %v of type %T\n", r, r)
 }
